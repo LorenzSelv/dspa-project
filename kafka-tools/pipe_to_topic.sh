3948355b091498748caa1242f52b57bfc7f14fdf
Lorenzo Selvatici <selvatici.lorenzo@gmail.com>
2019-03-12 20:16:05 +0100

    add example to populate stream

--- kafka-tools/pipe_to_topic.sh
+++ kafka-tools/pipe_to_topic.sh
@@ -1,1 +1,2 @@
+# USAGE EXAMPLE: tail -n +2 dataset/1k-users-sorted/streams/likes_event_stream.csv | head | kafka-tools/pipe_to_topic.sh likes
 $KAFKA/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic $1
