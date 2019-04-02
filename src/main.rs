5239154710ef724565fcde02f7443109e28eab38
decovas <sara.decova@gmail.com>
2019-04-02 12:13:16 +0200

    start work

--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,29 @@ fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
 
         worker.dataflow::<u64,_,_>(|scope| {
-            
-            let likes_stream = 
+            let likes_stream =
                 kafka::consumer::string_stream(scope, "likes")
                     .map(|record: String| event::deserialize::<LikeEvent>(record));
 
-            let comments_stream = 
+            let comments_stream =
                 kafka::consumer::string_stream(scope, "comments")
                     .map(|record: String| event::deserialize::<CommentEvent>(record));
 
-            let posts_stream = 
+            let posts_stream =
                 kafka::consumer::string_stream(scope, "posts")
                     .map(|record: String| event::deserialize::<PostEvent>(record));
 
             likes_stream
-                .inspect(|event: &LikeEvent| { println!("{:?}", event); });
+                .inspect(|event: &LikeEvent| { println!("{:?}", event); })
+                .broadcast();
             comments_stream
-                .inspect(|event: &CommentEvent| { println!("{:?}", event); });
+                .inspect(|event: &CommentEvent| { println!("{:?}", event); })
+                .broadcast();
             posts_stream
-                .inspect(|event: &PostEvent| { println!("{:?}", event); });
+                .inspect(|event: &PostEvent| { println!("{:?}", event); })
+                .exchange(|event: &PostEvent| event.post_ud);
         });
 
     }).expect("Timely computation failed somehow");
 }
+
